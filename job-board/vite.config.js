022bc4a24a1dc39a939b9a2626db3dda234de612
UlisesChopin16 <uliseschopin16@gmail.com>
2025-09-23 17:59:01 -0600

    Se hizo la configuración para tailwind

--- job-board/vite.config.js
+++ job-board/vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
+import tailwindcss from '@tailwindcss/vite';
+
 export default defineConfig({
     plugins: [
         laravel({
@@ -21,6 +23,7 @@ export default defineConfig({
                 },
             },
         }),
+        tailwindcss(),
     ],
     resolve: {
         alias: {
